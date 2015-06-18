3f99fde5ef282299a62518ea92121db1826b5f8a
aureliano <aureliano.franca@hotmail.com>
2015-06-18 14:13:00 -0300

    Create constraint violation model.

---
+++ src/test/java/com/github/aureliano/damihilogs/validation/ConstraintViolationTest.java
@@ -0,0 +1,33 @@
+package com.github.aureliano.damihilogs.validation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.github.aureliano.damihilogs.annotation.NotNull;
+
+public class ConstraintViolationTest {
+
+	@Test
+	public void testEquals() {
+		ConstraintViolation c1 = new ConstraintViolation();
+		Assert.assertFalse(c1.equals(null));
+		
+		ConstraintViolation c2 = new ConstraintViolation();
+		Assert.assertEquals(c1, c2);
+		
+		c1.withMessage("A test message.");
+		Assert.assertFalse(c1.equals(c2));
+		
+		c2.withMessage("A test message.");
+		Assert.assertTrue(c1.equals(c2));
+		
+		c1.withValidator(NotNull.class);
+		Assert.assertFalse(c1.equals(c2));
+		
+		c2.withValidator(Override.class);
+		Assert.assertFalse(c1.equals(c2));
+		
+		c2.withValidator(NotNull.class);
+		Assert.assertTrue(c1.equals(c2));
+	}
+}
