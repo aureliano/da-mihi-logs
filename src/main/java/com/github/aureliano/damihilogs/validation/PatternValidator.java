d0120e310e249c308cb168384471367e5115ac33
aureliano <aureliano.franca@hotmail.com>
2015-06-22 12:26:22 -0300

    Create pattern validation.

--- src/main/java/com/github/aureliano/damihilogs/validation/PatternValidator.java
+++ src/main/java/com/github/aureliano/damihilogs/validation/PatternValidator.java
@@ -19,17 +19,13 @@ public class PatternValidator implements IValidator {
 		String property = ReflectionHelper.getSimpleAccessMethodName(method);
 		Object returnedValue = ReflectionHelper.callMethod(configuration, method.getName(), null, null);
 		
-		if (returnedValue == null) {
-			returnedValue = "";
+		if ((returnedValue == null) || !(returnedValue instanceof String)) {
+			throw new DaMihiLogsException(Pattern.class.getName() + " can be applyed only for " +
+					String.class.getName() + " types.");
 		}
 		
 		String message = ((Pattern) annotation).message();
 		String regex = ((Pattern) annotation).value();
-		
-		if (!(returnedValue instanceof String)) {
-			throw new DaMihiLogsException(Pattern.class.getName() + " can be applyed only for " +
-					String.class.getName() + " types.");
-		}
 		
 		if (!returnedValue.toString().matches(regex)) {
 			return new ConstraintViolation()
