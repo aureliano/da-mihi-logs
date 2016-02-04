88fa3205c300bf125f56a7c855b63d5e879d1420
aureliano <aureliano.franca@hotmail.com>
2016-02-04 14:35:43 -0200

    Create input config types enum.

---
+++ alma-mater-core/src/main/java/com/github/aureliano/almamater/core/config/InputConfigTypes.java
@@ -0,0 +1,12 @@
+package com.github.aureliano.damihilogs.config.input;
+
+public enum InputConfigTypes {
+
+	EXTERNAL_COMMAND,
+	FILE_INPUT,
+	FILE_TAILER,
+	STANDARD_INPUT,
+	URL,
+	JDBC_INPUT,
+	TWITTER_INPUT;
+}
