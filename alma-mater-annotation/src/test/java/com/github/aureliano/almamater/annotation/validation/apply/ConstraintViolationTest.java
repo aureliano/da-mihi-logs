a8ada0a349ce82758c5b2fbd54aef073e5957c9f
aureliano <aureliano.franca@hotmail.com>
2016-02-04 17:38:03 -0200

    Create constration violation tester.

---
+++ alma-mater-annotation/src/test/java/com/github/aureliano/almamater/annotation/validation/apply/ConstraintViolationTest.java
@@ -0,0 +1,34 @@
+package com.github.aureliano.almamater.annotation.validation.apply;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ConstraintViolationTest {
+
+	@Test
+	public void testEquals() {
+		ConstraintViolation c1 = new ConstraintViolation();
+		assertFalse(c1.equals(null));
+		
+		ConstraintViolation c2 = new ConstraintViolation();
+		assertEquals(c1, c2);
+		
+		c1.withMessage("A test message.");
+		assertFalse(c1.equals(c2));
+		
+		c2.withMessage("A test message.");
+		assertTrue(c1.equals(c2));
+		
+		/*c1.withValidator(NotNull.class);
+		assertFalse(c1.equals(c2));
+		
+		c2.withValidator(Override.class);
+		assertFalse(c1.equals(c2));
+		
+		c2.withValidator(NotNull.class);
+		assertTrue(c1.equals(c2));*/
+	}
+}
