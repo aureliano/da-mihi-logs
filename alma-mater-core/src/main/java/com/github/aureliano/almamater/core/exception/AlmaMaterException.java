846093435e31604a0f23ebfe6ec8c389623688f7
aureliano <aureliano.franca@hotmail.com>
2016-02-04 17:32:42 -0200

    Create project exception.

---
+++ alma-mater-core/src/main/java/com/github/aureliano/almamater/core/exception/AlmaMaterException.java
@@ -0,0 +1,22 @@
+package com.github.aureliano.almamater.core.exception;
+
+public class AlmaMaterException extends RuntimeException {
+
+	private static final long serialVersionUID = -3189219196670479175L;
+
+	public AlmaMaterException() {
+		super();
+	}
+
+	public AlmaMaterException(String message) {
+		super(message);
+	}
+
+	public AlmaMaterException(Throwable cause) {
+		super(cause);
+	}
+
+	public AlmaMaterException(Throwable cause, String message) {
+		super(message, cause);
+	}
+}
