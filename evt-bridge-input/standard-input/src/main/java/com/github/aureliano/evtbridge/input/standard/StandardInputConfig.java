118d2f3f93f0ab2ab787942b3ad2c4bfb5c4f2fa
aureliano <aureliano.franca@hotmail.com>
2016-02-05 17:05:25 -0200

    Create standard input configuration.

---
+++ evt-bridge-input/standard-input/src/main/java/com/github/aureliano/evtbridge/input/standard/StandardInputConfig.java
@@ -0,0 +1,160 @@
+package com.github.aureliano.evtbridge.input.standard;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.github.aureliano.evtbridge.core.config.IConfigInput;
+import com.github.aureliano.evtbridge.core.config.InputConfigTypes;
+import com.github.aureliano.evtbridge.core.exception.IExceptionHandler;
+import com.github.aureliano.evtbridge.core.helper.DataHelper;
+import com.github.aureliano.evtbridge.core.listener.DataReadingListener;
+import com.github.aureliano.evtbridge.core.listener.ExecutionListener;
+import com.github.aureliano.evtbridge.core.matcher.IMatcher;
+
+public class StandardInputConfig implements IConfigInput {
+
+	private String encoding;
+	private String id;
+	private Boolean useLastExecutionRecords;
+	private Properties metadata;
+	private List<IExceptionHandler> exceptionHandlers;
+	private IMatcher matcher;
+	private List<DataReadingListener> dataReadingListeners;
+	private List<ExecutionListener> inputExecutionListeners;
+	
+	public StandardInputConfig() {
+		this.encoding = "UTF-8";
+		this.useLastExecutionRecords = false;
+		this.metadata = new Properties();
+		this.exceptionHandlers = new ArrayList<IExceptionHandler>();
+		
+		this.dataReadingListeners = new ArrayList<DataReadingListener>();
+		this.inputExecutionListeners = new ArrayList<ExecutionListener>();
+	}
+	
+	public String getEncoding() {
+		return encoding;
+	}
+	
+	public StandardInputConfig withEncoding(String encoding) {
+		this.encoding = encoding;
+		return this;
+	}
+
+	@Override
+	public String getConfigurationId() {
+		return id;
+	}
+
+	@Override
+	public StandardInputConfig withConfigurationId(String id) {
+		this.id = id;
+		return this;
+	}
+	
+	public IMatcher getMatcher() {
+		return matcher;
+	}
+	
+	public StandardInputConfig withMatcher(IMatcher matcher) {
+		this.matcher = matcher;
+		return this;
+	}
+	
+	@Override
+	public Boolean isUseLastExecutionRecords() {
+		return this.useLastExecutionRecords;
+	}
+
+	@Override
+	public StandardInputConfig withUseLastExecutionRecords(Boolean useLastExecutionRecords) {
+		this.useLastExecutionRecords = useLastExecutionRecords;
+		return this;
+	}
+
+	public List<DataReadingListener> getDataReadingListeners() {
+		return dataReadingListeners;
+	}
+
+	public StandardInputConfig withDataReadingListeners(List<DataReadingListener> dataReadingListeners) {
+		this.dataReadingListeners = dataReadingListeners;
+		return this;
+	}
+	
+	public StandardInputConfig addDataReadingListener(DataReadingListener listener) {
+		this.dataReadingListeners.add(listener);
+		return this;
+	}
+	
+	@Override
+	public StandardInputConfig clone() {
+		return new StandardInputConfig()
+			.withEncoding(this.encoding)
+			.withConfigurationId(this.id)
+			.withMatcher(this.matcher)
+			.withMetadata(DataHelper.copyProperties(this.metadata))
+			.withExceptionHandlers(this.exceptionHandlers)
+			.withDataReadingListeners(this.dataReadingListeners)
+			.withExecutionListeners(this.inputExecutionListeners);
+	}
+	
+	@Override
+	public StandardInputConfig putMetadata(String key, String value) {
+		this.metadata.put(key, value);
+		return this;
+	}
+	
+	@Override
+	public String getMetadata(String key) {
+		return this.metadata.getProperty(key);
+	}
+	
+	protected StandardInputConfig withMetadata(Properties properties) {
+		this.metadata = properties;
+		return this;
+	}
+	
+	protected StandardInputConfig withExceptionHandlers(List<IExceptionHandler> handlers) {
+		this.exceptionHandlers = handlers;
+		return this;
+	}
+
+	@Override
+	public Properties getMetadata() {
+		return this.metadata;
+	}
+
+	@Override
+	public StandardInputConfig addExceptionHandler(IExceptionHandler handler) {
+		this.exceptionHandlers.add(handler);
+		return this;
+	}
+
+	@Override
+	public List<IExceptionHandler> getExceptionHandlers() {
+		return this.exceptionHandlers;
+	}
+
+	@Override
+	public List<ExecutionListener> getExecutionListeners() {
+		return this.inputExecutionListeners;
+	}
+
+	@Override
+	public StandardInputConfig withExecutionListeners(List<ExecutionListener> inputExecutionListeners) {
+		this.inputExecutionListeners = inputExecutionListeners;
+		return this;
+	}
+
+	@Override
+	public StandardInputConfig addExecutionListener(ExecutionListener listener) {
+		this.inputExecutionListeners.add(listener);
+		return this;
+	}
+
+	@Override
+	public String id() {
+		return InputConfigTypes.STANDARD_INPUT.name();
+	}
+}
---
+++ evt-bridge-input/standard-input/src/test/java/com/github/aureliano/evtbridge/input/standard/StandardInputConfigTest.java
@@ -0,0 +1,44 @@
+package com.github.aureliano.evtbridge.input.standard;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.github.aureliano.evtbridge.core.config.IConfigInput;
+import com.github.aureliano.evtbridge.core.config.InputConfigTypes;
+import com.github.aureliano.evtbridge.core.exception.IExceptionHandler;
+
+public class StandardInputConfigTest {
+
+	@Test
+	public void testConfiguration() {
+		StandardInputConfig c = (StandardInputConfig) new StandardInputConfig()
+			.withEncoding("ISO-8859-1")
+			.withConfigurationId("standard.input.config");
+		
+		Assert.assertEquals("ISO-8859-1", c.getEncoding());
+		Assert.assertEquals("standard.input.config", c.getConfigurationId());
+	}
+	
+	@Test
+	public void testClone() {
+		StandardInputConfig c1 = (StandardInputConfig) new StandardInputConfig()
+			.withEncoding("ISO-8859-1")
+			.withConfigurationId("standard.input.config")
+			.putMetadata("test", "my test")
+			.addExceptionHandler(new IExceptionHandler() {
+				public void captureException(Runnable runnable, IConfigInput inputConfig, Throwable throwable) { }
+			});
+		
+		StandardInputConfig c2 = c1.clone();
+		
+		Assert.assertEquals(c1.getEncoding(), c2.getEncoding());
+		Assert.assertEquals(c1.getConfigurationId(), c2.getConfigurationId());
+		Assert.assertEquals(c1.getMetadata("test"), c2.getMetadata("test"));
+		Assert.assertEquals(c1.getExceptionHandlers().size(), c2.getExceptionHandlers().size());
+	}
+	
+	@Test
+	public void testInputType() {
+		Assert.assertEquals(InputConfigTypes.STANDARD_INPUT.name(), new StandardInputConfig().id());
+	}
+}
