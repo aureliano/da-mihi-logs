569c0f6396d07a2cfca320826c5329489287ebca
aureliano <aureliano.franca@hotmail.com>
2016-02-12 15:55:04 -0200

    User common reflections helper.

--- evt-bridge-core/src/main/java/com/github/aureliano/evtbridge/core/register/ApiServiceRegistrator.java
+++ evt-bridge-core/src/main/java/com/github/aureliano/evtbridge/core/register/ApiServiceRegistrator.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.github.aureliano.evtbridge.common.exception.EventBridgeException;
+import com.github.aureliano.evtbridge.common.helper.ReflectionHelper;
 import com.github.aureliano.evtbridge.common.helper.StringHelper;
 import com.github.aureliano.evtbridge.core.agent.IAgent;
 import com.github.aureliano.evtbridge.core.config.IConfiguration;
-import com.github.aureliano.evtbridge.core.helper.ReflectionHelper;
 
 public final class ApiServiceRegistrator {
 
