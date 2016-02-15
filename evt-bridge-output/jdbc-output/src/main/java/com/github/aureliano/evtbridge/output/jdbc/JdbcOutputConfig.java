2a8c254e749ee92d76cb3c38b472b3c63563295e
aureliano <aureliano.franca@hotmail.com>
2016-02-15 15:25:10 -0200

    Create jdbc output configuration.

---
+++ evt-bridge-output/jdbc-output/src/main/java/com/github/aureliano/evtbridge/output/jdbc/JdbcOutputConfig.java
@@ -0,0 +1,140 @@
+package com.github.aureliano.evtbridge.output.jdbc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.github.aureliano.evtbridge.annotation.validation.NotNull;
+import com.github.aureliano.evtbridge.annotation.validation.Valid;
+import com.github.aureliano.evtbridge.core.config.IConfigOutput;
+import com.github.aureliano.evtbridge.core.config.OutputConfigTypes;
+import com.github.aureliano.evtbridge.core.filter.IEventFielter;
+import com.github.aureliano.evtbridge.core.formatter.IOutputFormatter;
+import com.github.aureliano.evtbridge.core.helper.DataHelper;
+import com.github.aureliano.evtbridge.core.jdbc.JdbcConnectionModel;
+import com.github.aureliano.evtbridge.core.listener.DataWritingListener;
+import com.github.aureliano.evtbridge.core.parser.IParser;
+import com.github.aureliano.evtbridge.core.register.ApiServiceRegistrator;
+import com.github.aureliano.evtbridge.core.register.ServiceRegistration;
+
+public class JdbcOutputConfig implements IConfigOutput {
+
+	static {
+		ServiceRegistration registration = new ServiceRegistration()
+			.withId(OutputConfigTypes.STANDARD_OUTPUT.name())
+			.withAgent(JdbcDataWriter.class)
+			.withConfiguration(JdbcOutputConfig.class);
+		
+		ApiServiceRegistrator.instance().registrate(registration);
+	}
+	
+	private JdbcConnectionModel connection;
+	private IParser<?> parser;
+	private IEventFielter filter;
+	private Properties metadata;
+	private IOutputFormatter outputFormatter;
+	private List<DataWritingListener> dataWritingListeners;
+	
+	public JdbcOutputConfig() {
+		this.metadata = new Properties();
+		this.dataWritingListeners = new ArrayList<DataWritingListener>();
+	}
+
+	@Override
+	public JdbcOutputConfig putMetadata(String key, String value) {
+		this.metadata.put(key, value);
+		return this;
+	}
+
+	@Override
+	public String getMetadata(String key) {
+		return this.metadata.getProperty(key);
+	}
+
+	@Override
+	public Properties getMetadata() {
+		return this.metadata;
+	}
+
+	@Override
+	public String id() {
+		return OutputConfigTypes.JDBC_OUTPUT.name();
+	}
+
+	@Override
+	public IParser<?> getParser() {
+		return this.parser;
+	}
+
+	@Override
+	public JdbcOutputConfig withParser(IParser<?> parser) {
+		this.parser = parser;
+		return this;
+	}
+
+	@Override
+	public IEventFielter getFilter() {
+		return this.filter;
+	}
+
+	@Override
+	public JdbcOutputConfig withFilter(IEventFielter filter) {
+		this.filter = filter;
+		return this;
+	}
+
+	@Override
+	public IOutputFormatter getOutputFormatter() {
+		return this.outputFormatter;
+	}
+
+	@Override
+	public List<DataWritingListener> getDataWritingListeners() {
+		return this.dataWritingListeners;
+	}
+
+	@Override
+	public JdbcOutputConfig withDataWritingListeners(List<DataWritingListener> dataWritingListeners) {
+		this.dataWritingListeners = dataWritingListeners;
+		return this;
+	}
+
+	@Override
+	public JdbcOutputConfig addDataWritingListener(DataWritingListener listener) {
+		this.dataWritingListeners.add(listener);
+		return this;
+	}
+
+	@Override
+	public JdbcOutputConfig withOutputFormatter(IOutputFormatter outputFormatter) {
+		this.outputFormatter = outputFormatter;
+		return this;
+	}
+	
+	@NotNull
+	@Valid
+	public JdbcConnectionModel getConnection() {
+		return connection;
+	}
+	
+	public JdbcOutputConfig withConnection(JdbcConnectionModel connection) {
+		this.connection = connection;
+		return this;
+	}
+	
+	@Override
+	public JdbcOutputConfig clone() {
+		return new JdbcOutputConfig()
+			.withParser(this.parser)
+			.withFilter(this.filter)
+			.withOutputFormatter(this.outputFormatter)
+			.withDataWritingListeners(this.dataWritingListeners)
+			.withMetadata(DataHelper.copyProperties(this.metadata))
+			.withConnection((this.connection == null) ? null : this.connection.clone());
+	}
+	
+	protected JdbcOutputConfig withMetadata(Properties properties) {
+		this.metadata = properties;
+		return this;
+	}
+}
---
+++ evt-bridge-output/jdbc-output/src/test/java/com/github/aureliano/evtbridge/output/jdbc/JdbcOutputConfigTest.java
@@ -0,0 +1,120 @@
+package com.github.aureliano.evtbridge.output.jdbc;
+
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.github.aureliano.evtbridge.annotation.validation.NotEmpty;
+import com.github.aureliano.evtbridge.annotation.validation.NotNull;
+import com.github.aureliano.evtbridge.annotation.validation.apply.ConstraintViolation;
+import com.github.aureliano.evtbridge.annotation.validation.apply.ObjectValidator;
+import com.github.aureliano.evtbridge.core.config.OutputConfigTypes;
+import com.github.aureliano.evtbridge.core.jdbc.JdbcConnectionModel;
+
+public class JdbcOutputConfigTest {
+	
+	ObjectValidator validator = ObjectValidator.instance();
+	
+	@Test
+	public void testConfiguration() {
+		JdbcOutputConfig c = new JdbcOutputConfig().withConnection(this.createConnection());
+		
+		Assert.assertEquals("Px", c.getConnection().getPassword());
+		Assert.assertEquals("org.postgresql.Driver", c.getConnection().getDriver());
+		Assert.assertEquals("tomas_torquemada", c.getConnection().getUser());
+		Assert.assertEquals("insert into spanish", c.getConnection().getSql());
+	}
+	
+	@Test
+	public void testClone() {
+		JdbcOutputConfig c1 = new JdbcOutputConfig()
+			.putMetadata("test", "my test");
+		
+		JdbcOutputConfig c2 = c1.clone();
+		
+		Assert.assertEquals(c1.getMetadata("test"), c2.getMetadata("test"));
+	}
+	
+	@Test
+	public void testOutputType() {
+		Assert.assertEquals(OutputConfigTypes.JDBC_OUTPUT.name(), new JdbcOutputConfig().id());
+	}
+	
+	@Test
+	public void testValidation() {
+		JdbcOutputConfig c = this.createValidConfiguration();
+		Assert.assertTrue(this.validator.validate(c).isEmpty());
+		
+		this._testConnection();
+		this._testInvalidDriver();
+		this._testInvalidUser();
+		this._testInvalidUrl();
+	}
+	
+	private void _testConnection() {
+		JdbcOutputConfig c = this.createValidConfiguration().withConnection(null);
+		Set<ConstraintViolation> violations = this.validator.validate(c);
+		
+		Assert.assertTrue(violations.size() == 1);
+		Assert.assertEquals(NotNull.class, violations.iterator().next().getValidator());
+	}
+	
+	private void _testInvalidDriver() {
+		JdbcOutputConfig c = this.createValidConfiguration();
+		c.getConnection().withDriver(null);
+		Set<ConstraintViolation> violations = this.validator.validate(c);
+		
+		Assert.assertTrue(violations.size() == 1);
+		Assert.assertEquals(NotEmpty.class, violations.iterator().next().getValidator());
+		
+		c.getConnection().withDriver("");
+		violations = this.validator.validate(c);
+		
+		Assert.assertTrue(violations.size() == 1);
+		Assert.assertEquals(NotEmpty.class, violations.iterator().next().getValidator());
+	}
+	
+	private void _testInvalidUser() {
+		JdbcOutputConfig c = this.createValidConfiguration();
+		c.getConnection().withUser(null);
+		Set<ConstraintViolation> violations = this.validator.validate(c);
+		
+		Assert.assertTrue(violations.size() == 1);
+		Assert.assertEquals(NotEmpty.class, violations.iterator().next().getValidator());
+		
+		c.getConnection().withUser("");
+		violations = this.validator.validate(c);
+		
+		Assert.assertTrue(violations.size() == 1);
+		Assert.assertEquals(NotEmpty.class, violations.iterator().next().getValidator());
+	}
+	
+	private void _testInvalidUrl() {
+		JdbcOutputConfig c = this.createValidConfiguration();
+		c.getConnection().withUrl(null);
+		Set<ConstraintViolation> violations = this.validator.validate(c);
+		
+		Assert.assertTrue(violations.size() == 1);
+		Assert.assertEquals(NotEmpty.class, violations.iterator().next().getValidator());
+		
+		c.getConnection().withUrl("");
+		violations = this.validator.validate(c);
+		
+		Assert.assertTrue(violations.size() == 1);
+		Assert.assertEquals(NotEmpty.class, violations.iterator().next().getValidator());
+	}
+	
+	private JdbcOutputConfig createValidConfiguration() {
+		return new JdbcOutputConfig().withConnection(this.createConnection());
+	}
+
+	private JdbcConnectionModel createConnection() {
+		return new JdbcConnectionModel()
+			.withPassword("Px")
+			.withDriver("org.postgresql.Driver")
+			.withUser("tomas_torquemada")
+			.withUrl("url")
+			.withSql("insert into spanish");
+	}
+}
