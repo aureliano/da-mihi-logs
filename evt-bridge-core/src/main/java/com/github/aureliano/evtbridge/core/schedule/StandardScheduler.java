83602c23aff1614826fa578e7ff1121dd2cf6959
aureliano <aureliano.franca@hotmail.com>
2016-02-17 10:55:06 -0200

    Implement scheduler interface.

--- evt-bridge-core/src/main/java/com/github/aureliano/evtbridge/core/schedule/StandardScheduler.java
+++ evt-bridge-core/src/main/java/com/github/aureliano/evtbridge/core/schedule/StandardScheduler.java
@@ -3,15 +3,11 @@ package com.github.aureliano.evtbridge.core.schedule;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
-public abstract class StandardScheduler {
+public abstract class StandardScheduler implements IScheduler {
 
 	protected ScheduledExecutorService scheduledExecutorService;
 	
 	protected StandardScheduler() {
 		this.scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 	}
-	
-	public abstract void prepareSchedulingForExecution(Runnable runnable);
-	
-	public abstract String id();
 }
