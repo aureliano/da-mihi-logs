7ed47495e7b6221bdf64d907aaef6da098b05ab6
aureliano <aureliano.franca@hotmail.com>
2016-02-17 10:34:03 -0200

    Create scheduler types enum.

---
+++ evt-bridge-core/src/main/java/com/github/aureliano/evtbridge/core/schedule/SchedulerTypes.java
@@ -0,0 +1,8 @@
+package com.github.aureliano.evtbridge.core.schedule;
+
+public enum SchedulerTypes {
+
+	EXECUTE_ONCE_AT_SPECIFIC_TIME,
+	EXECUTE_PERIODICALLY_AT_SPECIFIC_TIME,
+	EXECUTE_PERIODICALLY;
+}
