9069328e1064b5122362aae5a78437f31a1c65f7
aureliano <aureliano.franca@hotmail.com>
2016-02-19 19:14:34 -0200

    Create standard input configuration parser.

---
+++ evt-bridge-input/standard-input/src/main/java/com/github/aureliano/evtbridge/input/standard/StandardInputConverter.java
@@ -0,0 +1,27 @@
+package com.github.aureliano.evtbridge.input.standard;
+
+import java.util.Map;
+
+import com.github.aureliano.evtbridge.core.config.AbstractConfigInputConverter;
+import com.github.aureliano.evtbridge.core.config.IConfigInput;
+import com.github.aureliano.evtbridge.core.config.InputConfigTypes;
+
+public class StandardInputConverter extends AbstractConfigInputConverter<IConfigInput> {
+
+	public StandardInputConverter() {
+		super();
+	}
+
+	@Override
+	public IConfigInput convert(Map<String, Object> data) {
+		StandardInputConfig conf = new StandardInputConfig();
+		super.configureObject(conf, data);
+		
+		return conf;
+	}
+	
+	@Override
+	public String id() {
+		return InputConfigTypes.STANDARD_INPUT.name();
+	}
+}
