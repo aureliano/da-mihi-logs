bf9f1aacbdbe8e6dcbbbcff1fd7f644ac0afaa53
aureliano <aureliano.franca@hotmail.com>
2016-02-22 10:25:51 -0300

    Handle input config conversion.

--- evt-bridge-converter/src/main/java/com/github/aureliano/evtbridge/converter/ConversionApplyer.java
+++ evt-bridge-converter/src/main/java/com/github/aureliano/evtbridge/converter/ConversionApplyer.java
@@ -3,7 +3,7 @@ package com.github.aureliano.evtbridge.converter;
 import java.util.List;
 import java.util.Map;
 
-import com.github.aureliano.evtbridge.common.exception.EventBridgeException;
+import com.github.aureliano.evtbridge.converter.exception.ConfigurationConverterException;
 
 public final class ConversionApplyer {
 
@@ -12,6 +12,10 @@ public final class ConversionApplyer {
 	}
 	
 	public static List<?> apply(ConverterType type, Map<String, Object> data) {
-		throw new EventBridgeException("Method not implemented.");
+		switch (type) {
+			case INPUT : return new InputConverter().convert(data);
+			//case OUTPUT : return new OutputConverter().convert(data);
+			default : throw new ConfigurationConverterException("Unsupported converter type.");
+		}
 	}
 }
