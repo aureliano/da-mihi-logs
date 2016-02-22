ce3e4b6aa94e9c41e8fdd12d5738593cf9bb1ea5
aureliano <aureliano.franca@hotmail.com>
2016-02-22 15:20:12 -0300

    Create conversion helper with parse json to configuration method.

---
+++ evt-bridge-converter/src/main/java/com/github/aureliano/evtbridge/converter/helper/ConversionHelper.java
@@ -0,0 +1,31 @@
+package com.github.aureliano.evtbridge.converter.helper;
+
+import java.util.Map;
+
+import com.github.aureliano.evtbridge.converter.ConfigurationConverter;
+import com.github.aureliano.evtbridge.converter.ConfigurationSourceType;
+import com.github.aureliano.evtbridge.converter.exception.ConfigurationConverterException;
+import com.github.aureliano.evtbridge.core.config.EventCollectorConfiguration;
+import com.github.aureliano.evtbridge.core.helper.DataHelper;
+import com.github.aureliano.evtbridge.core.helper.FileHelper;
+
+public final class ConversionHelper {
+
+	private ConversionHelper() {}
+	
+	public static EventCollectorConfiguration loadConfiguration(String path, ConfigurationSourceType type) {
+		switch (type) {
+			case YAML: return null;
+			case JSON: return convertFromJson(path);
+			default: throw new ConfigurationConverterException("Configuration source type '" + type + "' not supported.");
+		}
+	}
+	
+	private static EventCollectorConfiguration convertFromJson(String path) {
+		String text = FileHelper.readFile(path);
+		@SuppressWarnings("unchecked")
+		Map<String, Object> data = DataHelper.jsonStringToObject(text, Map.class);
+		
+		return new ConfigurationConverter().convert(data);
+	}
+}
