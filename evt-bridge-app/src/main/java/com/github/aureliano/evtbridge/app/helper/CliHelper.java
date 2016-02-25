05c753cdc3a226c6c82401cb28c1415921529952
aureliano <aureliano.franca@hotmail.com>
2016-02-25 17:50:03 -0300

    Add command line options to help and version.

--- evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/helper/CliHelper.java
+++ evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/helper/CliHelper.java
@@ -6,6 +6,9 @@ import java.util.List;
 import com.github.aureliano.evtbridge.common.helper.StringHelper;
 import com.github.aureliano.evtbridge.converter.ConfigurationSourceType;
 
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
 public final class CliHelper {
 
 	private CliHelper() {}
@@ -19,5 +22,14 @@ public final class CliHelper {
 		}
 		
 		return StringHelper.join(types, ", ");
+	}
+	
+	public static OptionSet buildOptionSet(String[] args) {
+		OptionParser parser = new OptionParser();
+		
+		parser.accepts("help", "Show this message");
+		parser.accepts("version", "Show project version");
+		
+		return parser.parse(args);
 	}
 }
--- evt-bridge-app/src/test/java/com/github/aureliano/evtbridge/app/helper/CliHelperTest.java
+++ evt-bridge-app/src/test/java/com/github/aureliano/evtbridge/app/helper/CliHelperTest.java
@@ -1,8 +1,11 @@
 package com.github.aureliano.evtbridge.app.helper;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import joptsimple.OptionSet;
 
 public class CliHelperTest {
 
@@ -12,5 +15,19 @@ public class CliHelperTest {
 		String actual = CliHelper.supportedFileTypes();
 		
 		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testBuildOptionSet() {
+		this._assertCliOption("h", "help");
+		this._assertCliOption("v", "version");
+	}
+	
+	private void _assertCliOption(String shortcut, String option) {
+		OptionSet options = CliHelper.buildOptionSet(new String[] { "-" + shortcut });
+		assertTrue(options.has(option));
+		
+		options = CliHelper.buildOptionSet(new String[] { "--" + option });
+		assertTrue(options.has(option));
 	}
 }
