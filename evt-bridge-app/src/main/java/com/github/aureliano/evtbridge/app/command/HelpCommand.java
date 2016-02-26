f58998239c2fbfb04c13c0b6401e2d5c3cbbf669
aureliano <aureliano.franca@hotmail.com>
2016-02-26 09:59:15 -0300

    Create help command.

---
+++ evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/command/HelpCommand.java
@@ -0,0 +1,10 @@
+package com.github.aureliano.evtbridge.app.command;
+
+public class HelpCommand implements ICommand {
+
+	public HelpCommand() {}
+	
+	public void execute() {
+		System.out.println("Help not supported yet.");
+	}
+}
