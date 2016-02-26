118592672696b0398de2ee113f44dfb36a03a8ff
aureliano <aureliano.franca@hotmail.com>
2016-02-26 10:14:23 -0300

    Version command implementation.

--- evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/command/VersionCommand.java
+++ evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/command/VersionCommand.java
@@ -1,10 +1,20 @@
 package com.github.aureliano.evtbridge.app.command;
 
+import com.github.aureliano.evtbridge.core.EventBridgeMetadata;
+
 public class VersionCommand implements ICommand {
 
 	public VersionCommand() {}
 	
 	public void execute() {
-		System.out.println("Version not supported yet.");
+		EventBridgeMetadata metadata = EventBridgeMetadata.instance();
+		
+		String versionMessage = String.format(
+			"%s (%s)",
+			metadata.getProperty("app.version"),
+			metadata.getProperty("app.release.date")
+		);
+		
+		System.out.println(versionMessage);
 	}
 }
