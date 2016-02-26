2d947c82e6585f4354f30f9d0ef2e99f0f4401f7
aureliano <aureliano.franca@hotmail.com>
2016-02-26 10:12:51 -0300

    Define version as 0.1.0:BETA-01

--- evt-bridge-core/src/test/java/com/github/aureliano/evtbridge/core/EventBridgeMetadataTest.java
+++ evt-bridge-core/src/test/java/com/github/aureliano/evtbridge/core/EventBridgeMetadataTest.java
@@ -22,7 +22,7 @@ public class EventBridgeMetadataTest {
 		assertEquals("https://github.com/aureliano/evt-bridge/wiki", metadata.getProperty("project.wiki"));
 		
 		assertEquals("1.7", metadata.getProperty("app.requirement.jvm"));
-		assertEquals("0.1.0", metadata.getProperty("app.version"));
+		assertEquals("0.1.0:BETA-01", metadata.getProperty("app.version"));
 		assertEquals("2016-02-25", metadata.getProperty("app.release.date"));
 	}
 }
