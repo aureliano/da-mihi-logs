8bf49720fff671bf59f20ad88be9fb26710b58ac
aureliano <aureliano.franca@hotmail.com>
2016-02-26 10:02:44 -0300

    Add methods to parse cli arguments and build command interfaces.

--- evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/helper/CliHelper.java
+++ evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/helper/CliHelper.java
@@ -3,6 +3,9 @@ package com.github.aureliano.evtbridge.app.helper;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.github.aureliano.evtbridge.app.command.HelpCommand;
+import com.github.aureliano.evtbridge.app.command.ICommand;
+import com.github.aureliano.evtbridge.app.command.VersionCommand;
 import com.github.aureliano.evtbridge.common.helper.StringHelper;
 import com.github.aureliano.evtbridge.converter.ConfigurationSourceType;
 
@@ -24,12 +27,36 @@ public final class CliHelper {
 		return StringHelper.join(types, ", ");
 	}
 	
-	public static OptionSet buildOptionSet(String[] args) {
+	public static ICommand buildCommand(String[] args) {
+		OptionSet options = CliHelper.buildOptionSet(args);
+		
+		if (options.has("help")) {
+			return new HelpCommand();
+		} else if (options.has("version")) {
+			return new VersionCommand();
+		}
+		
+		System.err.println("Don't know how to handle the command: " + StringHelper.join(args, " "));
+		return null;
+	}
+	
+	protected static OptionSet buildOptionSet(String[] args) {
 		OptionParser parser = new OptionParser();
 		
 		parser.accepts("help", "Show this message");
 		parser.accepts("version", "Show project version");
 		
 		return parser.parse(args);
+	}
+	
+	protected static ICommand buildCommand(String[] args, OptionSet options) {
+		if (options.has("help")) {
+			return new HelpCommand();
+		} else if (options.has("version")) {
+			return new VersionCommand();
+		}
+		
+		System.err.println("Don't know how to handle the command: " + StringHelper.join(args, " "));
+		return null;
 	}
 }
--- evt-bridge-app/src/test/java/com/github/aureliano/evtbridge/app/helper/CliHelperTest.java
+++ evt-bridge-app/src/test/java/com/github/aureliano/evtbridge/app/helper/CliHelperTest.java
@@ -1,9 +1,14 @@
 package com.github.aureliano.evtbridge.app.helper;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import com.github.aureliano.evtbridge.app.command.HelpCommand;
+import com.github.aureliano.evtbridge.app.command.ICommand;
+import com.github.aureliano.evtbridge.app.command.VersionCommand;
 
 import joptsimple.OptionSet;
 
@@ -21,6 +26,21 @@ public class CliHelperTest {
 	public void testBuildOptionSet() {
 		this._assertCliOption("h", "help");
 		this._assertCliOption("v", "version");
+	}
+	
+	@Test
+	public void testBuildCommand() {
+		OptionSet options = CliHelper.buildOptionSet(new String[0]);
+		ICommand command = CliHelper.buildCommand(new String[0], options);
+		assertNull(command);
+		
+		options = CliHelper.buildOptionSet(new String[] { "-h" });
+		command = CliHelper.buildCommand(new String[0], options);
+		assertTrue(command instanceof HelpCommand);
+		
+		options = CliHelper.buildOptionSet(new String[] { "-v" });
+		command = CliHelper.buildCommand(new String[0], options);
+		assertTrue(command instanceof VersionCommand);
 	}
 	
 	private void _assertCliOption(String shortcut, String option) {
