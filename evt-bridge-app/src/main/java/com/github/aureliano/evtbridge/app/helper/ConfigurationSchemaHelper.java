f5d204d6aae5224076e64f4a13204f44ea3c097b
aureliano <aureliano.franca@hotmail.com>
2016-02-26 14:29:50 -0300

    Add methods to load root schema and list configuration schema names.

--- evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/helper/ConfigurationSchemaHelper.java
+++ evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/helper/ConfigurationSchemaHelper.java
@@ -1,9 +1,43 @@
 package com.github.aureliano.evtbridge.app.helper;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.github.aureliano.evtbridge.core.helper.DataHelper;
+import com.github.aureliano.evtbridge.core.helper.FileHelper;
+
 public final class ConfigurationSchemaHelper {
 
 	private static final String CONFIGURATION_SCHEMA_DIR = "configuration-schema";
 	private static final String SCHEMA_NAME_SEPARATOR = "-";
+	private static final String ROOT_SCHEMA = "root-schema.json";
 	
 	private ConfigurationSchemaHelper() {}
+	
+	@SuppressWarnings("unchecked")
+	public static Map<String, Object> loadRootSchema() {
+		String path = FileHelper.buildPath(CONFIGURATION_SCHEMA_DIR, ROOT_SCHEMA);
+		String jsonString = FileHelper.readResource(path);
+		
+		return DataHelper.jsonStringToObject(jsonString, Map.class);
+	}
+	
+	public static List<String> fetchSchemaNames() {
+		Map<String, Object> map = ConfigurationSchemaHelper.loadRootSchema();
+		List<String> names = new ArrayList<String>();
+		names.add("root");
+		
+		for (String key : map.keySet()) {
+			if (key.equals("id")) {
+				continue;
+			}
+			
+			if (map.get(key) instanceof Map) {
+				names.add(key);
+			}
+		}
+		
+		return names;
+	}
 }
--- evt-bridge-app/src/test/java/com/github/aureliano/evtbridge/app/helper/ConfigurationSchemaHelperTest.java
+++ evt-bridge-app/src/test/java/com/github/aureliano/evtbridge/app/helper/ConfigurationSchemaHelperTest.java
@@ -1,5 +1,34 @@
 package com.github.aureliano.evtbridge.app.helper;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
 public class ConfigurationSchemaHelperTest {
 
+	@Test
+	public void testLoadRootSchema() {
+		Map<String, Object> map = ConfigurationSchemaHelper.loadRootSchema();
+		
+		assertEquals("event-collector-configuration.json", map.get("id"));
+		assertNotNull(map.get("event_collection_scheduler"));
+		assertNotNull(map.get("input"));
+		assertNotNull(map.get("output"));
+	}
+	
+	@Test
+	public void testFetchSchemaNames() {
+		List<String> schemata = ConfigurationSchemaHelper.fetchSchemaNames();
+		
+		assertEquals(4, schemata.size());
+		assertTrue(schemata.contains("root"));
+		assertTrue(schemata.contains("event_collection_scheduler"));
+		assertTrue(schemata.contains("input"));
+		assertTrue(schemata.contains("output"));
+	}
 }
