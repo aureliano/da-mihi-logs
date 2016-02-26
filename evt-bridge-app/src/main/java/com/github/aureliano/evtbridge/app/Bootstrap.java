d6e2de79a86443e9072b50ec1de7e09242c11c14
aureliano <aureliano.franca@hotmail.com>
2016-02-26 10:18:55 -0300

    Configure bootstrap class.

--- evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/Bootstrap.java
+++ evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/Bootstrap.java
@@ -1,8 +1,40 @@
 package com.github.aureliano.evtbridge.app;
 
-public class Bootstrap {
+import com.github.aureliano.evtbridge.app.command.HelpCommand;
+import com.github.aureliano.evtbridge.app.command.ICommand;
+import com.github.aureliano.evtbridge.app.helper.CliHelper;
 
+public class Bootstrap {
+	
+	public Bootstrap() {
+		super();
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Bootstraping Event Bridge!");
+		new Bootstrap().startUp(args);
+	}
+	
+	public void startUp(String[] args) {
+		if (args.length == 0) {
+			new HelpCommand().execute();;
+		} else {
+			this.executeCommand(args);
+		}
+	}
+	
+	private void executeCommand(String[] args) {
+		ICommand command = CliHelper.buildCommand(args);
+		if (command == null) {
+			this.exit(ErrorCode.COMMAND_NOT_FOUND);
+		}
+		
+		ErrorCode error = command.execute();
+		if (error != null) {
+			this.exit(error);
+		}
+	}
+	
+	private void exit(ErrorCode error) {
+		System.exit(error.getCode());
 	}
 }
