f2338f5eb8ecae6cd28eaef182799aaa894fd585
aureliano <aureliano.franca@hotmail.com>
2016-02-26 14:48:51 -0300

    Schemata command.

--- evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/command/SchemataCommand.java
+++ evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/command/SchemataCommand.java
@@ -1,13 +1,25 @@
 package com.github.aureliano.evtbridge.app.command;
 
+import java.util.List;
+
 import com.github.aureliano.evtbridge.app.ErrorCode;
+import com.github.aureliano.evtbridge.app.helper.ConfigurationSchemaHelper;
+import com.github.aureliano.evtbridge.common.helper.StringHelper;
 
 public class SchemataCommand implements ICommand {
 
 	public SchemataCommand() {}
 
 	public ErrorCode execute() {
-		throw new RuntimeException("Not implemented yet.");
+		List<String> schemata = ConfigurationSchemaHelper.fetchSchemaNames();
+		String message = new StringBuilder()
+			.append("Configuration schemata:")
+			.append("\n - ")
+			.append(StringHelper.join(schemata, "\n - "))
+			.toString();
+		
+		System.out.println(message);
+		return null;
 	}
 
 	public String id() {
