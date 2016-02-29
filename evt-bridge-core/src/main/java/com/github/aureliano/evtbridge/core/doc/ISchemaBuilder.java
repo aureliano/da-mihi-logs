e5971e2b5b09c39a106dc0fa6c3dc7c179463242
aureliano <aureliano.franca@hotmail.com>
2016-02-29 17:19:20 -0300

    Change build return type.

--- evt-bridge-core/src/main/java/com/github/aureliano/evtbridge/core/doc/ISchemaBuilder.java
+++ evt-bridge-core/src/main/java/com/github/aureliano/evtbridge/core/doc/ISchemaBuilder.java
@@ -6,5 +6,5 @@ import com.github.aureliano.evtbridge.core.SchemaTypes;
 
 public interface ISchemaBuilder {
 
-	public abstract Map<String, ?> build(SchemaTypes schemaType);
+	public abstract Map<String, Object> build(SchemaTypes schemaType);
 }
