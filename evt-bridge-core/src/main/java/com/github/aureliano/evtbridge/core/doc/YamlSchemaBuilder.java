c49631701a6f1f8b3a18a9a94ff66fac8620bdf3
aureliano <aureliano.franca@hotmail.com>
2016-02-29 13:55:12 -0300

    Create yaml schema builder.

---
+++ evt-bridge-core/src/main/java/com/github/aureliano/evtbridge/core/doc/YamlSchemaBuilder.java
@@ -0,0 +1,15 @@
+package com.github.aureliano.evtbridge.core.doc;
+
+import java.util.Map;
+
+import com.github.aureliano.evtbridge.core.SchemaTypes;
+
+public class YamlSchemaBuilder implements ISchemaBuilder {
+
+	public YamlSchemaBuilder() {}
+
+	@Override
+	public Map<String, ?> build(SchemaTypes schemaType) {
+		return null;
+	}
+}
