cfe55d08b68ec9dd41fb9ee2c1d30d313497afe6
aureliano <aureliano.franca@hotmail.com>
2016-02-29 13:54:54 -0300

    Create json schema builder.

---
+++ evt-bridge-core/src/main/java/com/github/aureliano/evtbridge/core/doc/JsonSchemaBuilder.java
@@ -0,0 +1,15 @@
+package com.github.aureliano.evtbridge.core.doc;
+
+import java.util.Map;
+
+import com.github.aureliano.evtbridge.core.SchemaTypes;
+
+public class JsonSchemaBuilder implements ISchemaBuilder {
+
+	public JsonSchemaBuilder() {}
+
+	@Override
+	public Map<String, ?> build(SchemaTypes schemaType) {
+		return null;
+	}
+}
