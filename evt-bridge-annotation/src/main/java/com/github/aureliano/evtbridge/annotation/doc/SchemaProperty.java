4edc9ead1f3890422170472cdb3a4616ac39f689
aureliano <aureliano.franca@hotmail.com>
2016-02-29 10:26:11 -0300

    Move schema property to doc package.

--- evt-bridge-annotation/src/main/java/com/github/aureliano/evtbridge/annotation/doc/SchemaProperty.java
+++ evt-bridge-annotation/src/main/java/com/github/aureliano/evtbridge/annotation/doc/SchemaProperty.java
@@ -1,4 +1,4 @@
-package com.github.aureliano.evtbridge.annotation;
+package com.github.aureliano.evtbridge.annotation.doc;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
