2d42fc5ce92b8b7dfa4de049b424798c8075afe2
aureliano <aureliano.franca@hotmail.com>
2016-03-01 14:29:00 -0300

    Schemata command.

--- evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/command/SchemataCommand.java
+++ evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/command/SchemataCommand.java
@@ -1,21 +1,21 @@
 package com.github.aureliano.evtbridge.app.command;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.github.aureliano.evtbridge.app.ErrorCode;
-import com.github.aureliano.evtbridge.app.helper.ConfigurationSchemaHelper;
 import com.github.aureliano.evtbridge.common.helper.StringHelper;
+import com.github.aureliano.evtbridge.core.SchemaTypes;
 
 public class SchemataCommand implements ICommand {
 
 	public SchemataCommand() {}
 
 	public ErrorCode execute() {
-		List<String> schemata = ConfigurationSchemaHelper.fetchSchemaNames();
 		String message = new StringBuilder()
 			.append("Configuration schemata:")
 			.append("\n - ")
-			.append(StringHelper.join(schemata, "\n - "))
+			.append(this.schemata())
 			.toString();
 		
 		System.out.println(message);
@@ -24,5 +24,16 @@ public class SchemataCommand implements ICommand {
 
 	public String id() {
 		return Commands.SCHEMATA.getId();
+	}
+	
+	private String schemata() {
+		SchemaTypes[] types = SchemaTypes.values();
+		List<String> schemata = new ArrayList<>(types.length);
+		
+		for (SchemaTypes type : types) {
+			schemata.add(type.name().toLowerCase());
+		}
+		
+		return StringHelper.join(schemata, "\n - ");
 	}
 }
