9a4f67c3dfbb27fdec69e2f0d102a4e23a5d4b05
aureliano <aureliano.franca@hotmail.com>
2016-03-01 18:25:51 -0300

    Schema command help.

--- evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/command/HelpCommand.java
+++ evt-bridge-app/src/main/java/com/github/aureliano/evtbridge/app/command/HelpCommand.java
@@ -3,6 +3,7 @@ package com.github.aureliano.evtbridge.app.command;
 import com.github.aureliano.evtbridge.app.ErrorCode;
 import com.github.aureliano.evtbridge.common.helper.StringHelper;
 import com.github.aureliano.evtbridge.core.EventBridgeMetadata;
+import com.github.aureliano.evtbridge.core.doc.DocumentationSourceTypes;
 import com.github.aureliano.evtbridge.core.helper.FileHelper;
 
 public class HelpCommand implements ICommand {
@@ -29,6 +30,8 @@ public class HelpCommand implements ICommand {
 	private ErrorCode commandHelp() {
 		if (Commands.SCHEMATA.name().equalsIgnoreCase(this.command)) {
 			this.printSchemataHelp();
+		} else if (Commands.SCHEMA.name().equalsIgnoreCase(this.command)) {
+			this.printSchemaHelp();
 		}
 		
 		return null;
@@ -39,13 +42,23 @@ public class HelpCommand implements ICommand {
 		System.out.println(this.replaceProperties(help));
 	}
 	
+	private void printSchemaHelp() {
+		String help = FileHelper.readResource("help/schema.help");
+		System.out.println(this.replaceProperties(help));
+	}
+	
 	private String replaceProperties(String help) {
 		EventBridgeMetadata metadata = EventBridgeMetadata.instance();
 		return help.replaceAll("\\$\\{app.binary.linux\\}", metadata.getProperty("app.binary.linux"))
 			.replaceAll("\\$\\{app.requirement.jvm\\}", metadata.getProperty("app.requirement.jvm"))
 			.replaceAll("\\$\\{project.name\\}", metadata.getProperty("project.name"))
 			.replaceAll("\\$\\{project.issues\\}", metadata.getProperty("project.issues"))
-			.replaceAll("\\$\\{project.wiki\\}", metadata.getProperty("project.wiki"));
+			.replaceAll("\\$\\{project.wiki\\}", metadata.getProperty("project.wiki"))
+			.replaceAll("\\$\\{format.types\\}", this.formatTypes());
+	}
+	
+	private String formatTypes() {
+		return StringHelper.join(DocumentationSourceTypes.values(), ", ").toLowerCase();
 	}
 	
 	public HelpCommand withCommand(String command) {
