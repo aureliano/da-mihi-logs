8adc1bd6974233ae6779a1ee5e0af5b3219d33e9
aureliano <aureliano.franca@hotmail.com>
2016-03-01 19:37:40 -0300

    Add method to build json schema input.

--- evt-bridge-core/src/test/java/com/github/aureliano/evtbridge/core/doc/JsonSchemaBuilderTest.java
+++ evt-bridge-core/src/test/java/com/github/aureliano/evtbridge/core/doc/JsonSchemaBuilderTest.java
@@ -44,4 +44,24 @@ public class JsonSchemaBuilderTest {
 		assertTrue(json.contains("\"execute_periodically_at_specific_time\""));
 		assertTrue(json.contains("\"execute_periodically\""));
 	}
+
+	@Test
+	public void testBuildInput() {
+		String json = new JsonSchemaBuilder().build(SchemaTypes.INPUT);
+
+		assertTrue(json.contains("http://json-schema.org/draft-04/schema#"));
+		assertTrue(json.contains("Input configuration."));
+		assertTrue(json.contains("object"));
+		
+		assertTrue(json.contains("  \"properties\" : {"));
+		
+		assertTrue(json.contains("    \"type\" : {"));
+		assertTrue(json.contains("      \"anyOf\" : ["));
+		assertTrue(json.contains("\"external_command\""));
+		assertTrue(json.contains("\"file\""));
+		assertTrue(json.contains("\"file_tailer\""));
+		assertTrue(json.contains("\"standard\""));
+		assertTrue(json.contains("\"url\""));
+		assertTrue(json.contains("\"jdbc\""));
+	}
 }
