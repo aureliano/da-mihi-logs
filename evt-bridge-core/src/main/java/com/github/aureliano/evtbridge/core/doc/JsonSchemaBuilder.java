fd29fcfb7131230d7962d7aa3bebef2d96695b87
aureliano <aureliano.franca@hotmail.com>
2016-03-01 13:42:51 -0300

    Code refactoring: use map schema builder.

--- evt-bridge-core/src/main/java/com/github/aureliano/evtbridge/core/doc/JsonSchemaBuilder.java
+++ evt-bridge-core/src/main/java/com/github/aureliano/evtbridge/core/doc/JsonSchemaBuilder.java
@@ -2,34 +2,36 @@ package com.github.aureliano.evtbridge.core.doc;
 
 import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.aureliano.evtbridge.common.exception.EventBridgeException;
 import com.github.aureliano.evtbridge.core.SchemaTypes;
-import com.github.aureliano.evtbridge.core.config.EventCollectorConfiguration;
-import com.github.aureliano.evtbridge.core.config.IConfiguration;
+import com.github.aureliano.evtbridge.core.data.ObjectMapperSingleton;
 
 public class JsonSchemaBuilder extends SchemaBuilder<String> {
 
-	public JsonSchemaBuilder() {}
+	private MapSchemaBuilder builder;
+	
+	public JsonSchemaBuilder() {
+		this.builder = new MapSchemaBuilder();
+	}
 
 	@Override
 	public String build(SchemaTypes schemaType) {
 		switch (schemaType) {
 		case ROOT:
-			return buildRootSchema().toString();
+			return this.convertMapToJson(this.builder.build(schemaType));
 		default:
 			throw new EventBridgeException("Unsupported schema type '" + schemaType + "'");
 		}
 	}
-
-	private Map<String, Object> buildRootSchema() {
-		if (super.schema != null) {
-			super.schema.clear();
+	
+	private String convertMapToJson(Map<String, Object> map) {
+		ObjectMapper mapper = ObjectMapperSingleton.instance().getObjectMapper();
+		try {
+			return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+		} catch (JsonProcessingException ex) {
+			throw new EventBridgeException(ex);
 		}
-		
-		Class<? extends IConfiguration> configuration = EventCollectorConfiguration.class;
-		super.configureSchemaHeader(configuration);
-		super.configureSchemaProperties(configuration);
-		
-		return super.schema;
 	}
 }
--- evt-bridge-core/src/test/java/com/github/aureliano/evtbridge/core/doc/JsonSchemaBuilderTest.java
+++ evt-bridge-core/src/test/java/com/github/aureliano/evtbridge/core/doc/JsonSchemaBuilderTest.java
@@ -11,20 +11,20 @@ public class JsonSchemaBuilderTest {
 	@Test
 	public void testBuildRoot() {
 		String json = new JsonSchemaBuilder().build(SchemaTypes.ROOT);
-		
+
 		assertTrue(json.contains("http://json-schema.org/draft-04/schema#"));
 		assertTrue(json.contains("Events collector configuration schema."));
 		assertTrue(json.contains("object"));
 		
-		assertTrue(json.contains("properties"));
+		assertTrue(json.contains("\"properties\" : {"));
 		
-		assertTrue(json.contains("collectorId"));
-		assertTrue(json.contains("metadata"));
-		assertTrue(json.contains("eventsCollectorListeners"));
-		assertTrue(json.contains("scheduler"));
-		assertTrue(json.contains("multiThreadingEnabled"));
-		assertTrue(json.contains("persistExecutionLog"));
-		assertTrue(json.contains("outputConfigs"));
-		assertTrue(json.contains("inputConfigs"));
+		assertTrue(json.contains("\"collectorId\" : {"));
+		assertTrue(json.contains("\"metadata\" : {"));
+		assertTrue(json.contains("\"eventsCollectorListeners\" : {"));
+		assertTrue(json.contains("\"scheduler\" : {"));
+		assertTrue(json.contains("\"multiThreadingEnabled\" : {"));
+		assertTrue(json.contains("\"persistExecutionLog\" : {"));
+		assertTrue(json.contains("\"outputConfigs\" : {"));
+		assertTrue(json.contains("\"inputConfigs\" : {"));
 	}
 }
