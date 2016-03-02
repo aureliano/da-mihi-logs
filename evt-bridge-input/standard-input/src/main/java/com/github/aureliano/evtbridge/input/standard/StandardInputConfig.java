ec9b5c234d63b79556c335add2aa832ae4460765
aureliano <aureliano.franca@hotmail.com>
2016-03-02 17:33:37 -0300

    Add doc annotations to standard input configuration.

--- evt-bridge-input/standard-input/src/main/java/com/github/aureliano/evtbridge/input/standard/StandardInputConfig.java
+++ evt-bridge-input/standard-input/src/main/java/com/github/aureliano/evtbridge/input/standard/StandardInputConfig.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import com.github.aureliano.evtbridge.annotation.doc.SchemaConfiguration;
+import com.github.aureliano.evtbridge.annotation.doc.SchemaProperty;
 import com.github.aureliano.evtbridge.core.config.IConfigInput;
 import com.github.aureliano.evtbridge.core.config.InputConfigTypes;
 import com.github.aureliano.evtbridge.core.exception.IExceptionHandler;
@@ -14,6 +16,11 @@ import com.github.aureliano.evtbridge.core.matcher.IMatcher;
 import com.github.aureliano.evtbridge.core.register.ApiServiceRegistrator;
 import com.github.aureliano.evtbridge.core.register.ServiceRegistration;
 
+@SchemaConfiguration(
+	schema = "http://json-schema.org/draft-04/schema#",
+	title = "Input configuration to consume from standard input (stdin).",
+	type = "object"
+)
 public class StandardInputConfig implements IConfigInput {
 
 	static {
@@ -55,6 +62,13 @@ public class StandardInputConfig implements IConfigInput {
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "configurationId",
+		types = "string",
+		description = "Input configuration id.",
+		defaultValue = "Auto-generated id.",
+		required = false
+	)
 	public String getConfigurationId() {
 		return id;
 	}
@@ -64,7 +78,13 @@ public class StandardInputConfig implements IConfigInput {
 		this.id = id;
 		return this;
 	}
-	
+
+	@SchemaProperty(
+		property = "matcher",
+		types = "string",
+		description = "Fully qualified name of class matcher used to get text from input.",
+		required = false
+	)
 	public IMatcher getMatcher() {
 		return matcher;
 	}
@@ -75,6 +95,12 @@ public class StandardInputConfig implements IConfigInput {
 	}
 	
 	@Override
+	@SchemaProperty(
+		property = "useLastExecutionLog",
+		types = "boolean",
+		description = "Whether to use data from earlier execution or not.",
+		defaultValue = "false"
+	)
 	public Boolean isUseLastExecutionRecords() {
 		return this.useLastExecutionRecords;
 	}
@@ -85,6 +111,12 @@ public class StandardInputConfig implements IConfigInput {
 		return this;
 	}
 
+	@SchemaProperty(
+		property = "dataReadingListeners",
+		types = "array",
+		description = "Fully qualified name of the class that will listen and fire an event before and after a log event is read.",
+		required = false
+	)
 	public List<DataReadingListener> getDataReadingListeners() {
 		return dataReadingListeners;
 	}
@@ -133,6 +165,12 @@ public class StandardInputConfig implements IConfigInput {
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "metadata",
+		types = "object",
+		description = "A key-value pair (hash <string, string>) which provides a mechanism to exchange metadata between configurations (main, inputs and outputs).",
+		required = false
+	)
 	public Properties getMetadata() {
 		return this.metadata;
 	}
@@ -144,11 +182,23 @@ public class StandardInputConfig implements IConfigInput {
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "exceptionHandlers",
+		types = "array",
+		description = "Fully qualified name of the class that will handle exceptions.",
+		required = false
+	)
 	public List<IExceptionHandler> getExceptionHandlers() {
 		return this.exceptionHandlers;
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "executionListeners",
+		types = "array",
+		description = "Fully qualified name of the class that will listen and fire an event before and after data reading execution.",
+		required = false
+	)
 	public List<ExecutionListener> getExecutionListeners() {
 		return this.inputExecutionListeners;
 	}
