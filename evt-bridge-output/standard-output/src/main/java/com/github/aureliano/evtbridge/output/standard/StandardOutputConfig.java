edf6dc6174c03399f8099954d2a6cfaf22c15d4c
aureliano <aureliano.franca@hotmail.com>
2016-03-03 09:55:54 -0300

    Add doc annotations to standard output configuration.

--- evt-bridge-output/standard-output/src/main/java/com/github/aureliano/evtbridge/output/standard/StandardOutputConfig.java
+++ evt-bridge-output/standard-output/src/main/java/com/github/aureliano/evtbridge/output/standard/StandardOutputConfig.java
@@ -4,16 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import com.github.aureliano.evtbridge.annotation.doc.SchemaConfiguration;
+import com.github.aureliano.evtbridge.annotation.doc.SchemaProperty;
 import com.github.aureliano.evtbridge.core.config.IConfigOutput;
 import com.github.aureliano.evtbridge.core.config.OutputConfigTypes;
+import com.github.aureliano.evtbridge.core.filter.EmptyFilter;
 import com.github.aureliano.evtbridge.core.filter.IEventFielter;
 import com.github.aureliano.evtbridge.core.formatter.IOutputFormatter;
+import com.github.aureliano.evtbridge.core.formatter.PlainTextFormatter;
 import com.github.aureliano.evtbridge.core.helper.DataHelper;
 import com.github.aureliano.evtbridge.core.listener.DataWritingListener;
 import com.github.aureliano.evtbridge.core.parser.IParser;
+import com.github.aureliano.evtbridge.core.parser.PlainTextParser;
 import com.github.aureliano.evtbridge.core.register.ApiServiceRegistrator;
 import com.github.aureliano.evtbridge.core.register.ServiceRegistration;
 
+@SchemaConfiguration(
+	schema = "http://json-schema.org/draft-04/schema#",
+	title = "Output configuration to print events to standard output (stdout).",
+	type = "object"
+)
 public class StandardOutputConfig implements IConfigOutput {
 
 	static {
@@ -35,6 +45,10 @@ public class StandardOutputConfig implements IConfigOutput {
 	public StandardOutputConfig() {
 		this.metadata = new Properties();
 		this.dataWritingListeners = new ArrayList<>();
+		
+		this.parser = new PlainTextParser();
+		this.filter = new EmptyFilter();
+		this.outputFormatter = new PlainTextFormatter();
 	}
 
 	@Override
@@ -43,6 +57,13 @@ public class StandardOutputConfig implements IConfigOutput {
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "parser",
+		types = "string",
+		description = "Fully qualified name of parser class used to convert an event into an business object.",
+		required = false,
+		defaultValue = "com.github.aureliano.evtbridge.core.parser.PlainTextParser"
+	)
 	public IParser<?> getParser() {
 		return this.parser;
 	}
@@ -54,6 +75,13 @@ public class StandardOutputConfig implements IConfigOutput {
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "filter",
+		types = "string",
+		description = "Fully qualified name of filter class used to filter events before writing.",
+		required = false,
+		defaultValue = "com.github.aureliano.evtbridge.core.filter.EmptyFilter"
+	)
 	public IEventFielter getFilter() {
 		return this.filter;
 	}
@@ -65,6 +93,13 @@ public class StandardOutputConfig implements IConfigOutput {
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "outputFormatter",
+		types = "string",
+		description = "Fully qualified name of formatter class used to format data output.",
+		required = false,
+		defaultValue = "com.github.aureliano.evtbridge.core.formatter.PlainTextFormatter"
+	)
 	public IOutputFormatter getOutputFormatter() {
 		return outputFormatter;
 	}
@@ -74,7 +109,13 @@ public class StandardOutputConfig implements IConfigOutput {
 		this.outputFormatter = outputFormatter;
 		return this;
 	}
-	
+
+	@SchemaProperty(
+		property = "dataWritingListeners",
+		types = "array",
+		description = "Fully qualified name of the class that will listen and fire an event before and after a log event is writen.",
+		required = false
+	)
 	public List<DataWritingListener> getDataWritingListeners() {
 		return dataWritingListeners;
 	}
@@ -115,6 +156,12 @@ public class StandardOutputConfig implements IConfigOutput {
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "metadata",
+		types = "object",
+		description = "A key-value pair (hash <string, string>) which provides a mechanism to exchange metadata between configurations (main, inputs and outputs).",
+		required = false
+	)
 	public Properties getMetadata() {
 		return this.metadata;
 	}
