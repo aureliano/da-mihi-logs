4efb07746e50b81af95fa6b03e687427f667ae29
aureliano <aureliano.franca@hotmail.com>
2016-03-03 10:10:49 -0300

    Add doc annotations to elastic search output configuration.

--- evt-bridge-output/elasticsearch-output/src/main/java/com/github/aureliano/evtbridge/output/elasticsearch/ElasticSearchOutputConfig.java
+++ evt-bridge-output/elasticsearch-output/src/main/java/com/github/aureliano/evtbridge/output/elasticsearch/ElasticSearchOutputConfig.java
@@ -4,17 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import com.github.aureliano.evtbridge.annotation.doc.SchemaConfiguration;
+import com.github.aureliano.evtbridge.annotation.doc.SchemaProperty;
 import com.github.aureliano.evtbridge.annotation.validation.NotEmpty;
 import com.github.aureliano.evtbridge.core.config.IConfigOutput;
 import com.github.aureliano.evtbridge.core.config.OutputConfigTypes;
+import com.github.aureliano.evtbridge.core.filter.EmptyFilter;
 import com.github.aureliano.evtbridge.core.filter.IEventFielter;
 import com.github.aureliano.evtbridge.core.formatter.IOutputFormatter;
+import com.github.aureliano.evtbridge.core.formatter.PlainTextFormatter;
 import com.github.aureliano.evtbridge.core.helper.DataHelper;
 import com.github.aureliano.evtbridge.core.listener.DataWritingListener;
 import com.github.aureliano.evtbridge.core.parser.IParser;
+import com.github.aureliano.evtbridge.core.parser.PlainTextParser;
 import com.github.aureliano.evtbridge.core.register.ApiServiceRegistrator;
 import com.github.aureliano.evtbridge.core.register.ServiceRegistration;
 
+@SchemaConfiguration(
+	schema = "http://json-schema.org/draft-04/schema#",
+	title = "Output configuration to save data onto ElasticSearch.",
+	type = "object"
+)
 public class ElasticSearchOutputConfig implements IConfigOutput, IElasticSearchConfiguration {
 
 	static {
@@ -45,9 +55,20 @@ public class ElasticSearchOutputConfig implements IConfigOutput, IElasticSearchC
 		this.printElasticSearchLog = DEFAULT_PRINT_ELASTIC_SEARCH_LOG;
 		this.metadata = new Properties();
 		this.dataWritingListeners = new ArrayList<DataWritingListener>();
+		
+		this.parser = new PlainTextParser();
+		this.filter = new EmptyFilter();
+		this.outputFormatter = new PlainTextFormatter();
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "host",
+		types = "string",
+		description = "ElasticSearch server host.",
+		required = false,
+		defaultValue = "localhost"
+	)
 	public String getHost() {
 		return host;
 	}
@@ -59,6 +80,13 @@ public class ElasticSearchOutputConfig implements IConfigOutput, IElasticSearchC
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "port",
+		types = "integer",
+		description = "ElasticSearch connection port.",
+		required = false,
+		defaultValue = "9200"
+	)
 	public int getPort() {
 		return port;
 	}
@@ -71,6 +99,12 @@ public class ElasticSearchOutputConfig implements IConfigOutput, IElasticSearchC
 
 	@Override
 	@NotEmpty
+	@SchemaProperty(
+		property = "index",
+		types = "string",
+		description = "The ElasticSearch index name.",
+		required = true
+	)
 	public String getIndex() {
 		return index;
 	}
@@ -83,6 +117,12 @@ public class ElasticSearchOutputConfig implements IConfigOutput, IElasticSearchC
 
 	@Override
 	@NotEmpty
+	@SchemaProperty(
+		property = "mappingType",
+		types = "string",
+		description = "The index's mapping type.",
+		required = true
+	)
 	public String getMappingType() {
 		return type;
 	}
@@ -94,6 +134,13 @@ public class ElasticSearchOutputConfig implements IConfigOutput, IElasticSearchC
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "printElasticSearchLog",
+		types = "boolean",
+		description = "Whether to print ElasticSearch log or not.",
+		required = false,
+		defaultValue = "false"
+	)
 	public boolean isPrintElasticSearchLog() {
 		return this.printElasticSearchLog;
 	}
@@ -110,6 +157,13 @@ public class ElasticSearchOutputConfig implements IConfigOutput, IElasticSearchC
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "parser",
+		types = "string",
+		description = "Fully qualified name of parser class used to convert an event into an business object.",
+		required = false,
+		defaultValue = "com.github.aureliano.evtbridge.core.parser.PlainTextParser"
+	)
 	public IParser<?> getParser() {
 		return this.parser;
 	}
@@ -121,6 +175,13 @@ public class ElasticSearchOutputConfig implements IConfigOutput, IElasticSearchC
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "filter",
+		types = "string",
+		description = "Fully qualified name of filter class used to filter events before writing.",
+		required = false,
+		defaultValue = "com.github.aureliano.evtbridge.core.filter.EmptyFilter"
+	)
 	public IEventFielter getFilter() {
 		return this.filter;
 	}
@@ -132,6 +193,13 @@ public class ElasticSearchOutputConfig implements IConfigOutput, IElasticSearchC
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "outputFormatter",
+		types = "string",
+		description = "Fully qualified name of formatter class used to format data output.",
+		required = false,
+		defaultValue = "com.github.aureliano.evtbridge.core.formatter.PlainTextFormatter"
+	)
 	public IOutputFormatter getOutputFormatter() {
 		return outputFormatter;
 	}
@@ -141,7 +209,13 @@ public class ElasticSearchOutputConfig implements IConfigOutput, IElasticSearchC
 		this.outputFormatter = outputFormatter;
 		return this;
 	}
-	
+
+	@SchemaProperty(
+		property = "dataWritingListeners",
+		types = "array",
+		description = "Fully qualified name of the class that will listen and fire an event before and after a log event is writen.",
+		required = false
+	)
 	public List<DataWritingListener> getDataWritingListeners() {
 		return dataWritingListeners;
 	}
@@ -188,6 +262,12 @@ public class ElasticSearchOutputConfig implements IConfigOutput, IElasticSearchC
 	}
 
 	@Override
+	@SchemaProperty(
+		property = "metadata",
+		types = "object",
+		description = "A key-value pair (hash <string, string>) which provides a mechanism to exchange metadata between configurations (main, inputs and outputs).",
+		required = false
+	)
 	public Properties getMetadata() {
 		return this.metadata;
 	}
